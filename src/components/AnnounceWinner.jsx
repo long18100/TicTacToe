ae56f4c18ad547a17f3f999f0f345ecb80785b92
Nguyen Duc Long <dlong.work@gmail.com>
2024-01-29 21:45:08 +0700

    update announce

--- src/components/AnnounceWinner.jsx
+++ src/components/AnnounceWinner.jsx
@@ -4,8 +4,7 @@ const AnnounceWinner = (props) => {
     return (
         <div className={props.className}>
             <div className="announce-winner_box">
-                <h3>{props.ntf}</h3>
-                <div>{props.value}</div>
+                <h3>{props.value}</h3>
                 <ResetButton onClick= {props.onClick}></ResetButton>
             </div>
         </div>
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -8,27 +8,28 @@ import User from "./User"
 const Game = () => {
     const [board, setBoard] = useState(Array(9).fill(null))
     const [isX, setIsX] = useState(true)
+    const [count, setCount] = useState(0)
     const winner = checkWin(board)
     const handleClick = (index) => {
         const boardCopy = [...board]
 
         if (winner || boardCopy[index]) return;
-
+        setCount(count + 1)
         boardCopy[index] = isX ? 'X' : 'O';
         setBoard(boardCopy);
         setIsX(!isX)
     }
-    console.log(winner)
     const handleReset = () => {
         setBoard(Array(9).fill(null))
         setIsX(true)
+        setCount(0)
     }
     return (
         <div className="game">
             <h1>TIC - TAC - TOE</h1>
             <ResetButton onClick={handleReset}></ResetButton>
             <Board onClick={handleClick} squares={board}></Board>
-            <AnnounceWinner onClick ={handleReset} className= {winner ? "announce-winner d-block" : "d-none"} value={winner ? `Winner is ${winner}`: "No Winner"}></AnnounceWinner>
+            <AnnounceWinner onClick ={handleReset} className= {winner && board!=null || count==9 ? "announce-winner d-block" : "d-none"} value={winner ? `Winner is ${winner}`: "No Winner"}></AnnounceWinner>
             <div className="user-container">
                 <User avatar="../public/user_1.jpg" className = {isX==true? "user active" : "user"}></User>
                 <User avatar="../public/user_2.jpg" className = {isX==false? "user active" : "user"}></User>
